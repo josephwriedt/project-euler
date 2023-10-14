a50d359ce33d6396d61d49cb6d7d8fe31613b721
Joseph Wriedt <joseph.wriedt@gmail.com>
2023-10-13 23:50:14 -0500

    Finish Problem 3

--- completed/problem-3.py
+++ completed/problem-3.py
@@ -1,9 +1,13 @@
 # Largest Prime Factor
 
+from typing import List, Dict
+
 def prime_number(n):
     x = 2
 
-    not_prime = set()
+    # keys = numbers
+    # values = list of prime multiples that are the prime factors of key
+    not_prime: Dict[List] = {}
 
     yield x
     while x * x <= n:
@@ -12,9 +16,12 @@ def prime_number(n):
             # yield prime
             yield x
 
-            # update the not prime set with multipules of prime (sieve of eratosthenes)
-            not_prime.update(range(x, n, x))
+            not_prime[x * x] = [x]
+        else:
+            for mult in not_prime[x]:
+                not_prime.setdefault(x + mult, []).append(mult)
 
+            del not_prime[x]
         x += 1
 
     yield False
